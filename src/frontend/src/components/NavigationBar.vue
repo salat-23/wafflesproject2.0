3aaf116896bbb22e118154017d31dee664960940
salat-23 <misikon@mail.ru>
2022-02-07 13:21:35 +0300

    Changed css to scss

--- src/frontend/src/components/NavigationBar.vue
+++ src/frontend/src/components/NavigationBar.vue
@@ -12,37 +12,8 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
-@import "/src/assets/palette.less";
-@import "/src/assets/adaptive-navbar.css";
+<style scoped lang="scss">
 
-.nav-bar {
-  height: 140px;
-  position: relative;
-  background: @palette-second;
-  border-bottom: 15px @palette-forth solid;
-}
 
-ul {
-
-  width: 100%;
-  position: absolute;
-  list-style-type: none;
-  bottom: 0;
-}
-
-li {
-  float: left;
-  margin-right: 20px;
-}
-
-@brs: 10px;
-a {
-  color: @palette-third;
-  padding: 10px;
-  font-size: @fs-big;
-  background: @palette-forth;
-  border-radius: @brs @brs 0 0;
-}
 
 </style>
