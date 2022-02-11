64df0bb07d9ba2e51458a86b13c8300bcb456846
salat23 <misikon@mail.ru>
2022-02-11 20:16:03 +0300

    Cleaned

--- src/main/java/com/salat23/wafflesfullstack/entity/RoleEnum.java
+++
@@ -1,7 +0,0 @@
-package com.salat23.wafflesfullstack.entity;
-
-public enum RoleEnum {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- src/main/java/com/salat23/wafflesfullstack/payload/request/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.salat23.wafflesfullstack.payload.request;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
